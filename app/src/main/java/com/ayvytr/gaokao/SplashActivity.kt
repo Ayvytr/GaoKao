033716082bcd5d8e26c0611d6f632b34ce09acb2
wangdunwei <wangdunwei@wxbtc.com>
2019-05-14 15:42:15 +0800

    修改启动页配置

--- app/src/main/java/com/ayvytr/gaokao/SplashActivity.kt
+++ app/src/main/java/com/ayvytr/gaokao/SplashActivity.kt
@@ -1,13 +1,27 @@
 package com.ayvytr.gaokao
 
 import android.support.v7.app.AppCompatActivity
+import io.reactivex.Observable
+import io.reactivex.disposables.Disposable
 import org.jetbrains.anko.startActivity
+import java.util.concurrent.TimeUnit
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var mDisposable: Disposable
 
     override fun onStart() {
         super.onStart()
-        startActivity<MainActivity>()
-        finish()
+        mDisposable = Observable.timer(500, TimeUnit.MILLISECONDS)
+            .subscribe {
+                startActivity<MainActivity>()
+                finish()
+            }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (!mDisposable.isDisposed) {
+            mDisposable.dispose()
+        }
     }
 }
