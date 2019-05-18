2e28ac0d1f646a947ef5ba431f7f369023900d93
Ayvytr <ayvytr@outlook.com>
2019-05-18 22:11:17 +0800

    1.配置Markdown

---
+++ app/src/main/java/com/ayvytr/gaokao/GkApplication.kt
@@ -0,0 +1,25 @@
+package com.ayvytr.gaokao
+
+import android.app.Application
+import com.alibaba.android.arouter.launcher.ARouter
+import com.ayvytr.commonlibrary.AppConfig
+import com.ayvytr.network.ApiClient
+
+/**
+ * @author admin
+ */
+class GkApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        init()
+    }
+
+    private fun init() {
+        if (AppConfig.isDebug) {
+            ARouter.openDebug()
+            ARouter.openLog()
+        }
+        ARouter.init(this)
+        ApiClient.getInstance().init(applicationContext, "https://github.com/ayvytr/")
+    }
+}
